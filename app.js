f47d9f7ffd042c5af0596bf3d0cee591b9dd7cd4
Denish Suthar <denishsuthar04@gmail.com>
2023-07-11 19:59:09 +0530

    bug fixed

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
-import express  from "express";
+import express from "express";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import ErrorMiddelware from "./middelware/error.js";
-import userRoute from "./routes/userRoute.js"
+import userRoute from "./routes/userRoute.js";
 import bodyParser from "body-parser";
-import postRoute from "./routes/postRoute.js"
-
-
+import postRoute from "./routes/postRoute.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser.json())
-
+app.use(bodyParser.json());
 // Config
 config({
-    path:"./config/config.env"
-})
+  path: "./config/config.env",
+});
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods:["GET", "POST", "PUT", "DELETE"]
+  })
+);
 
-app.get("/", (req, res)=>{
-    res.send("Welcome")
-})
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
 
 // Using Route
-app.use("/api/v1", userRoute)
-app.use("/api/v1", postRoute)
+app.use("/api/v1", userRoute);
+app.use("/api/v1", postRoute);
 
-
-
-export default app
+export default app;
 
 // Using ErrorMiddelware
 app.use(ErrorMiddelware);
--- controller/userController.js
+++ controller/userController.js
@@ -156,7 +156,7 @@ export const forgotPassword = catchAsyncError(async(req, res,next)=>{
 
     await user.save({ validateBeforeSave: false });
 
-    const resetPasswordUrl = `${req.protocol}://${req.get("host")}/api/v1/password/reset/${resetToken}`;
+    const resetPasswordUrl = `${process.env.FRONTEND_URL}/api/v1/password/reset/${resetToken}`;
 
     const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\nIf you have not requested this email then, please ignore it.`;
 
